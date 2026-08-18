4de2429fb42623e8b34a2126db2d52cd4c3afc62
Alexandra Marques <prof.alexandra.marques@gmail.com>
2026-08-18 10:44:07 -0300

    Add files via upload

---
+++ area_triangulo.py
@@ -0,0 +1,8 @@
+#Área do Triângulo
+#Ler da tela os dados
+base = float(input("Digite a base: "))
+altura = float(input("Digite a altura: "))
+#Calculo da área
+area = (base*altura)/2
+#Mostrar na tela
+print("A área do triângulo é = {:.2f}".format(area))
