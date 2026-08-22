0d1b245d6d10ea2ede2232b7e8cde9584dc945d9
Alexandra Marques <prof.alexandra.marques@gmail.com>
2026-08-21 21:20:47 -0300

    Add files via upload

---
+++ multiplicacao.py
@@ -0,0 +1,8 @@
+#Multiplicar dois números
+#Ler dois números
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+#Cálculo da Multiplicação
+mult = n1 * n2
+#Mostra na tela o resultado
+print("A multiplicação de",n1,"*",n2,"=",mult)
