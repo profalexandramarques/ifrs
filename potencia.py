d5b455dbde39629b5dbae162a5cce43510b7c218
Alexandra Marques <prof.alexandra.marques@gmail.com>
2026-08-21 21:22:11 -0300

    Add files via upload

---
+++ potencia.py
@@ -0,0 +1,8 @@
+#Potência dois números
+#Ler dois números
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+#Cálculo da Potencia
+potencia = n1 ** n2
+#Mostra na tela o resultado
+print("A potência de",n1,"**",n2,"=",potencia)
